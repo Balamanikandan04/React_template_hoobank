cd19cf7e7a6c84a405e51780715cba8b6f2dfaa4
Balamanikandan M <127951067+Balamanikandan04@users.noreply.github.com>
2024-02-12 23:27:15 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Navbar  from "./Navbar";
+import Business from "./Business";
+import Billing from "./Billing";
+import Carddeal from "./Carddeal";
+import Hero  from "./Hero";
+import Testimonial from "./Testimonial";
+import CTA  from "./CTA";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";
+import Buttons from "./Buttons";
+import Clients from "./Clients";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+export {
+Navbar, Business, Carddeal, Hero, Testimonial,CTA, GetStarted, Stats, Buttons, Clients, FeedbackCard, Footer,Billing
+};
